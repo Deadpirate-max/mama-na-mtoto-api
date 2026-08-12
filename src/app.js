dccac237003313df57a40a5471b8a02b314b7b87
Your Name <youremail@example.com>
2026-08-12 19:35:59 +0300

    Add registrations routes and mount to app

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const helmet = require("helmet");
 const rateLimit = require("express-rate-limit");
 
 const registrationsRoutes = require('./routes/registrationsRoutes');
-app.use('/registrations', registrationsRoutes);
+
 const authRoutes = require("./routes/authRoutes");
 const mothersRoutes = require("./routes/mothersRoutes");
 const alertsRoutes = require("./routes/alertsRoutes");
@@ -16,6 +16,7 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
+app.use('/registrations', registrationsRoutes);
 
 const adminRoutes = require('./routes/adminRoutes');
 app.use('/admin', adminRoutes);
